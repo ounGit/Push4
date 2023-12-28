4fc157cf487d91e0b489c70a8ddbd33e63f0e061
Mr. Oun <>
2023-12-28 11:36:07 +0700

    This is push4 not completed yet.

---
+++ src/push4/Push4.java
@@ -0,0 +1,8 @@
+package push4;
+public class Push4 {
+    public static void main(String[] args) {
+       hello everyone you are my love I love all you and teacher so much. Don'n' forget say you love me too. 
+       Thank you.
+    }
+    
+}
